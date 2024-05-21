6043c1cbb3764eab489735a53529670241445b4b
Suzuki <tejc210240@gmail.com>
2024-05-21 11:41:20 +0900

    kadai6

---
+++ src/main/java/jp/te4a/spring/boot/myapp6/Mybootapp6.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybootapp6 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mybootapp6.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mybootapp6/Mybootapp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybootapp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybootapp6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
