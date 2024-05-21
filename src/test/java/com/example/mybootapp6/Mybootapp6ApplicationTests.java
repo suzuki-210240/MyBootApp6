54b8518052e99be31caab2365bb1032609159ba4
Suzuki <tejc210240@gmail.com>
2024-05-21 11:44:36 +0900

    修正

--- src/test/java/com/example/mybootapp6/Mybootapp6ApplicationTests.java
+++ src/test/java/com/example/mybootapp6/Mybootapp6ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.mybootapp6;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Mybootapp6ApplicationTests {
 
 	@Test
