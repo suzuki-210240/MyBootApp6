f13ddd1eefb3f5668c87b228e569348761a8aa7b
Suzuki <tejc210240@gmail.com>
2024-05-24 12:44:22 +0900

    kadai8

--- src/main/java/jp/te4a/spring/boot/myapp8/BookRepository.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/BookRepository.java
@@ -1,17 +1,28 @@
-package jp.te4a.spring.boot.myapp7;
+package jp.te4a.spring.boot.myapp8;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-
 import org.springframework.stereotype.Repository;
+import org.springframework.beans.BeanUtils;
 
 @Repository
 public class BookRepository {
     private final ConcurrentMap<Integer,BookBean>bookMap = new ConcurrentHashMap<>();
+    
+    private int BOOK_ID = 0;
+    public int getBookId(){
+        return BOOK_ID++;
+    }
 
-    public BookBean save(BookBean bookBean){
+    public BookBean create(BookBean bookBean){
         return bookMap.put(bookBean.getId(),bookBean);
+    }
+
+    public BookBean update(BookBean updatBookBean){
+        BookBean bookBean = bookMap.get(updatBookBean.getId());
+        BeanUtils.copyProperties(updatBookBean, bookBean);
+        return bookBean;
     }
 
     public void delete(Integer bookId){
@@ -20,6 +31,10 @@ public class BookRepository {
 
     public List<BookBean> findAll(){
         return new ArrayList<>(bookMap.values());
+    }
+
+    public BookBean findOne(Integer id){
+        return bookMap.get(id);
     }
 }
 
