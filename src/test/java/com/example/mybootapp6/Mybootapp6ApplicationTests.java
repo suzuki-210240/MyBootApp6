f8e1bcc4d6fcb9938951c0343a738cda63b78a75
Suzuki <tejc210240@gmail.com>
2024-08-27 12:57:26 +0900

    修正

--- src/test/java/com/example/mybootapp6/Mybootapp6ApplicationTests.java
+++ src/test/java/com/example/mybootapp6/Mybootapp6ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.mybootapp6;
+/*package com.example.mybootapp6;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class Mybootapp6ApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
